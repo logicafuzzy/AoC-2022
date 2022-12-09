5ef1893cb475404e80d76f7c25334e04f83987e9
Michele Antolini <michele.antolini.it@gmail.com>
2022-12-09 23:05:05 +0100

    Day 00 - prototype

---
+++ Day00/Day00.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << " AoC 2022 Day00" << endl;
+
+	ifstream input("Day00.txt");
+
+	while (input) {
+		string line;
+		getline(input, line);
+
+		if (line.length() == 0)
+			break;
+	}
+
+	input.close();
+
+	return 0;
+}
