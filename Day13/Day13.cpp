5a25af681cb77cb8b1bfad309133768e5d9a7761
Michele Antolini <mantolini@seavision-group.com>
2023-11-30 14:22:26 +0100

    part 13 - comparision (tbc)

--- Day13/Day13.cpp
+++ Day13/Day13.cpp
@@ -3,26 +3,24 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <tuple>
+#include <cassert>
 
 using namespace std;
 
 class list {
 public:
 	list(int number) : _number(number), _isNumber(true) {}
-	list(vector<list> list) : _number(0), _isNumber(false), _subList(list) {}
-
-private:
-	bool _isNumber{ true };
-	int _number{ 0 };
-	vector<list> _subList;
+	list(std::initializer_list<list> list) : _number(0), _isNumber(false), _subList(list) {}
 
 	int compare(const list& other) {
 		if (_isNumber && other._isNumber) {
+			cout << "compare " << _number << " with " << other._number << endl;
 			return _number == other._number ? 0 : _number < other._number ? -1 : 1;
 		}
 		else if (!_isNumber && !other._isNumber) {
 			for (int i = 0; i < min(_subList.size(), other._subList.size()); ++i) {
-				int comp = _subList[i].compare(other);
+				int comp = _subList[i].compare(other._subList[i]);
 				if (comp == 0)
 					continue;
 				else
@@ -35,33 +33,96 @@ private:
 				return _subList.size() > other._subList.size() ? 1 : -1;
 		}
 		else if (_isNumber) {
-			list converted({ _number });
+			list converted{ {_number} };
 			return converted.compare(other);
 		}
 		else {
-			list converted({ other._number });
+			list converted{ {other._number} };
 			return compare(converted);
 		}
 
 	}
+private:
+	bool _isNumber{ true };
+	int _number{ 0 };
+	std::vector<list> _subList;
 };
 
+//list make_list(std::string string) {
+//	
+//}
 
 void test() {
-
-}
-
-int main() {
-	cout << " AoC 2022 Day13" << endl;
+	cout << " AoC 2022 Day13 test" << endl;
 
 	ifstream input("Day13test.txt");
 
-	while (input) {
-		string line;
-		getline(input, line);
+	vector<bool> testResults{ true, true, false, true, false, true, false, false };
 
-		if (line.length() == 0)
-			break;
+	constexpr bool isTest = true;
+	int testIndex = 0;
+	int count = 0;
+	int index = 1;
+
+	vector<list> first({ {1,1,3,1,1}, {{{1}},{2,3,4}}, {{9}} });
+	vector<list> second({ {1,1,5,1,1}, {{{1}}, 4}, {{8,7,6}} });
+
+	assert(first.size() == second.size());
+
+	for (int i = 0; i < first.size(); ++i) {
+
+		bool result = first[i].compare(second[i]) == -1;
+
+		count += result ? index : 0;
+
+		if (isTest)
+			cout << "result: " << std::boolalpha << result << " passed: " << (result == testResults[testIndex++]) << endl;
+
+		index++;
+	}
+
+	cout << "right order index sum: " << count << endl;
+	input.close();
+}
+
+int main() {
+	cout << " AoC 2022 Day13 test" << endl;
+
+	test();
+
+	return 0;
+	ifstream input("Day13.txt");
+
+	vector<bool> testResults{ true, true, false, true, false, true, false, false };
+
+	constexpr bool isTest = true;
+	int testIndex = 0;
+	int count = 0;
+	int index = 1;
+
+	vector<list> first{ {1,1,3,1,1}, {{1},{2,3,4}} };
+	vector<list> second({ {1,1,5,1,1} });
+
+	while (input) {
+		string line1, line2, ignore;
+		getline(input, line1);
+		getline(input, line2);
+		getline(input, ignore);
+
+		cout << line1 << endl;
+		cout << line2 << endl;
+
+		bool result = line1.compare(line2) == -1;
+		
+		count += result ? index : 0;
+
+		if (isTest)
+			cout << "result: " << (result == testResults[testIndex++]) << endl;
+
+		cout << "right order index sum: " << count;
+
+		index++;
+
 	}
 
 	input.close();
