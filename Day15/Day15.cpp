d368c099f679e6a38b5de54d49b34ab3780d6187
Michele Antolini <michele.antolini.it@gmail.com>
2023-12-03 22:15:10 +0100

    Day15 part2 WIP

--- Day15/Day15.cpp
+++ Day15/Day15.cpp
@@ -22,8 +22,16 @@ int xmax{ std::numeric_limits<int>::min() };
 int ymin{ std::numeric_limits<int>::max() };
 int ymax{ std::numeric_limits<int>::min() };
 
-//constexpr int check_row = 10;
+#define TEST
+#define PART2
+
+#ifdef TEST
+constexpr int check_row = 10;
+constexpr int max_coord = 20;
+#else
 constexpr int check_row = 2000000;
+constexpr int max_coord = 4000000;
+#endif
 
 void print_map(map_type& map, int xmin, int xmax, int ymin, int ymax) {
 	for (int y = ymin; y <= ymax; ++y) {
@@ -58,13 +66,34 @@ void update_minmax(int value, int& min, int& max) {
 	}
 }
 
+#ifdef PART2
+void add_line(map_type& map, int x1, int y1, int x2, int y2) {
+	assert(x1 == x2 || y1 == y2);
+
+	if (x1 == x2) {
+		if (min(y1, y2) >= 0 && max(y1, y2) <= max_coord)
+			for (int y = min(y1, y2); y <= max(y1, y2); ++y) {
+				if (map.find(y) == map.end() || map[y].find(x1) == map[y].end())
+					map[y][x1] = cell_type::no_beacon;
+			}
+	}
+	else {
+		if (y1 < 0 || y1 > max_coord)
+			return;
+		for (int x = min(x1, x2); x <= max(x1, x2); ++x) {
+			if (map.find(y1) == map.end() || map[y1].find(x) == map[y1].end())
+				map[y1][x] = cell_type::no_beacon;
+		}
+	}
+}
+#else
 void add_line(map_type& map, int x1, int y1, int x2, int y2) {
 	assert(x1 == x2 || y1 == y2);
 
 	if (x1 == x2) {
 		if (min(y1, y2) <= check_row && max(y1, y2) >= check_row &&
 			(map.find(check_row) == map.end() || map[check_row].find(x1) == map[check_row].end()))
-				map[check_row][x1] = cell_type::no_beacon;
+			map[check_row][x1] = cell_type::no_beacon;
 	}
 	else {
 		if (y1 != check_row)
@@ -75,6 +104,7 @@ void add_line(map_type& map, int x1, int y1, int x2, int y2) {
 		}
 	}
 }
+#endif
 
 void update_area(map_type& map, int sensor_x, int sensor_y, int beacon_x, int beacon_y) {
 	int manhattan_distance = abs(beacon_x - sensor_x) + abs(beacon_y - sensor_y);
@@ -91,12 +121,13 @@ void update_area(map_type& map, int sensor_x, int sensor_y, int beacon_x, int be
 		add_line(map, sensor_x, sensor_y - i, sensor_x - manhattan_distance + i, sensor_y - i);
 	}
 
+#ifdef TEST
 	update_minmax(sensor_x - manhattan_distance, xmin, xmax);
 	update_minmax(sensor_x + manhattan_distance, xmin, xmax);
 
 	update_minmax(sensor_y - manhattan_distance, ymin, ymax);
 	update_minmax(sensor_y + manhattan_distance, ymin, ymax);
-
+#endif
 }
 
 int get_no_beacon_in_row(map_type& map, int y) {
@@ -110,7 +141,11 @@ int get_no_beacon_in_row(map_type& map, int y) {
 int main() {
 	cout << " AoC 2022 Day15" << endl;
 
+#ifdef TEST
+	ifstream input("Day15test.txt");
+#else
 	ifstream input("Day15.txt");
+#endif
 
 	map_type map;
 
@@ -127,11 +162,13 @@ int main() {
 		map[sensor_y][sensor_x] = cell_type::sensor;
 		map[beacon_y][beacon_x] = cell_type::beacon;
 
+#ifdef TEST
 		update_minmax(sensor_x, xmin, xmax);
 		update_minmax(beacon_x, xmin, xmax);
 
 		update_minmax(sensor_y, ymin, ymax);
 		update_minmax(beacon_y, ymin, ymax);
+#endif
 
 		//if (sensor_x == 8 && sensor_y == 7)
 			update_area(map, sensor_x, sensor_y, beacon_x, beacon_y);
@@ -144,10 +181,40 @@ int main() {
 
 	printf("\n");
 
-	//print_map(map, xmin, xmax, ymin, ymax);
+#ifdef TEST
+	#ifdef PART2
+		print_map(map, 0, max_coord, 0, max_coord);
+	#else
+		print_map(map, xmin, xmax, ymin, ymax);
+	#endif
+#endif
+	
+#ifdef PART2
+	int beacon_x, beacon_y;
 
+	//find row with only one empty
+	for (auto& row : map) {
+		beacon_y = row.first;
+		if (beacon_y < 0 || beacon_y > max_coord)
+			continue;
+		int count = 0;
+		int cell_xmin = 0;
+		int cell_xmax = numeric_limits<int>::max();
+		for (auto& cell : row.second) {
+			beacon_x = cell.first;
+			if (beacon_x < 0 || beacon_x > max_coord)
+				continue;
+			update_minmax(beacon_x, cell_xmin, cell_xmax);
+			++count;
+		}
+		printf("Row %d count: %d\n", beacon_y, count);
+		if (count == cell_xmax - cell_xmin - 1)
+			break;
+	}
+	printf("Beacon in %d, %d frequency: %d", beacon_x, beacon_y, beacon_x * 4000000 + beacon_y);
+#else
 	cout << "No beacons in row " << check_row << ": " << get_no_beacon_in_row(map, check_row) << endl;
-
+#endif
 
 	return 0;
 }
